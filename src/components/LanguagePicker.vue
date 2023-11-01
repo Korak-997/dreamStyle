0c2cc2886122f3e3b54a60c703843452d6be875d
Korak Kurani <korak.kurani@sincnovation.com>
2023-11-01 10:37:38 +0100

    Fix some styles

--- src/components/LanguagePicker.vue
+++ src/components/LanguagePicker.vue
@@ -1,41 +1,43 @@
 <script>
-import { useI18n } from "vue-i18n";
-import Tr from "../i18n/translations";
-export default {
-  name: "UiSettings",
-  setup() {
-    const { t, locale } = useI18n();
-    const supportedLocales = Tr.supportedLocales;
-    const switchLanguage = async (newLocale) => {
-      await Tr.switchLanguage(newLocale);
-    };
-    return { t, locale, supportedLocales, switchLanguage };
-  },
-  data() {
-    const savedLocale = JSON.parse(localStorage.getItem("alphaSalon"));
-    return {
-      locale:
-        savedLocale == null
-          ? import.meta.env.VITE_DEFAULT_LOCALE
-          : savedLocale.settings.locale,
-    };
-  },
-  methods: {
-    saveChanges(newLocale) {
-      localStorage.setItem(
-        "alphaSalon",
-        JSON.stringify({ settings: { locale: newLocale } }),
-      );
-
-      this.$router.go("");
+  import { useI18n } from "vue-i18n";
+  import Tr from "../i18n/translations";
+  export default {
+    name: "UiSettings",
+    setup() {
+      const { t, locale } = useI18n();
+      const supportedLocales = Tr.supportedLocales;
+      const switchLanguage = async (newLocale) => {
+        await Tr.switchLanguage(newLocale);
+      };
+      return { t, locale, supportedLocales, switchLanguage };
     },
-  },
-};
+    data() {
+      const savedLocale = JSON.parse(localStorage.getItem("alphaSalon"));
+      return {
+        locale:
+          savedLocale == null
+            ? import.meta.env.VITE_DEFAULT_LOCALE
+            : savedLocale.settings.locale,
+      };
+    },
+    methods: {
+      saveChanges(newLocale) {
+        localStorage.setItem(
+          "alphaSalon",
+          JSON.stringify({ settings: { locale: newLocale } })
+        );
+
+        this.$router.go("");
+      },
+    },
+  };
 </script>
 
 <template>
   <div class="dropdown dropdown-end text-center">
-    <label tabindex="0" class="btn btn-ghost rounded-btn hover:border-primary"
+    <label
+      tabindex="0"
+      class="btn btn-primary btn-ghost rounded-btn"
       ><img
         :src="`/flags/${locale}.svg`"
         :alt="`Country flag for (${sLocale}) language`"
@@ -45,10 +47,10 @@ export default {
     /></label>
     <ul
       tabindex="0"
-      class="menu dropdown-content p-2 shadow bg-base-300 w-36 flex flex-col justify-around items-center"
+      class="menu dropdown-content p-2 shadow bg-base-100 w-36 flex flex-col justify-around items-center"
     >
       <li
-        class="w-full text-center border-b border-neutral hover:bg-secondary"
+        class="w-full text-center border-b border-neutral hover:bg-accent"
         v-for="sLocale in supportedLocales"
         :key="`locale-${sLocale}`"
         @click="
