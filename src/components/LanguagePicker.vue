85267e5f869bea25251d4fac2140ebc18e2195bb
Korak Kurani <korak.kurani@sincnovation.com>
2023-11-03 13:48:58 +0100

    Make language picker look better

--- src/components/LanguagePicker.vue
+++ src/components/LanguagePicker.vue
@@ -1,48 +1,52 @@
 <script>
-import { useI18n } from "vue-i18n";
-import Tr from "../i18n/translations";
-export default {
-  name: "UiSettings",
-  setup() {
-    const { t, locale } = useI18n();
-    const supportedLocales = Tr.supportedLocales;
-    const switchLanguage = async (newLocale) => {
-      await Tr.switchLanguage(newLocale);
-    };
-    return { t, locale, supportedLocales, switchLanguage };
-  },
-  data() {
-    const savedLocale = JSON.parse(localStorage.getItem("alphaSalon"));
-    return {
-      locale:
-        savedLocale == null
-          ? import.meta.env.VITE_DEFAULT_LOCALE
-          : savedLocale.settings.locale,
-    };
-  },
-  methods: {
-    saveChanges(newLocale) {
-      localStorage.setItem(
-        "alphaSalon",
-        JSON.stringify({ settings: { locale: newLocale } }),
-      );
-
-      this.$router.go("");
+  import { useI18n } from "vue-i18n";
+  import Tr from "../i18n/translations";
+  export default {
+    name: "UiSettings",
+    setup() {
+      const { t, locale } = useI18n();
+      const supportedLocales = Tr.supportedLocales;
+      const switchLanguage = async (newLocale) => {
+        await Tr.switchLanguage(newLocale);
+      };
+      return { t, locale, supportedLocales, switchLanguage };
     },
-  },
-};
+    data() {
+      const savedLocale = JSON.parse(localStorage.getItem("alphaSalon"));
+      return {
+        locale:
+          savedLocale == null
+            ? import.meta.env.VITE_DEFAULT_LOCALE
+            : savedLocale.settings.locale,
+      };
+    },
+    methods: {
+      saveChanges(newLocale) {
+        localStorage.setItem(
+          "alphaSalon",
+          JSON.stringify({ settings: { locale: newLocale } })
+        );
+
+        this.$router.go("");
+      },
+    },
+  };
 </script>
 
 <template>
   <div class="dropdown dropdown-end text-center">
-    <label tabindex="0" class="btn btn-primary btn-ghost rounded-btn"
-      ><img
+    <div
+      tabindex="0"
+      class="mx-4 hover:shadow-sm rounded-xl hover:shadow-primary hover:cursor-pointer"
+    >
+      <img
         :src="`/flags/${locale}.svg`"
         :alt="`Country flag for (${sLocale}) language`"
         class="w-8 rounded-xl"
         width="32px"
         height="32px"
-    /></label>
+      />
+    </div>
     <ul
       tabindex="0"
       class="menu dropdown-content p-2 shadow bg-base-100 w-36 flex flex-col justify-around items-center"
