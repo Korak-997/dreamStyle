05a762e6308d893a6f7443fa4411b641835f1d9a
Korak Kurani <korak.kurani@sincnovation.com>
2024-01-31 07:44:22 +0100

    Make about page look better

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -1,81 +1,46 @@
 <script>
-import BookNowBtn from "../components/BookNowBtn.vue";
+  import BookNowBtn from "../components/BookNowBtn.vue";
 
-export default {
-  name: "AboutPage",
-  data() {
-    return {};
-  },
-  components: {
-    BookNowBtn,
-  },
-  methods: {},
-};
+  export default {
+    name: "AboutPage",
+    data() {
+      return {};
+    },
+    components: {
+      BookNowBtn,
+    },
+    methods: {},
+  };
 </script>
 <template>
   <div class="flex flex-col items-center justify-center gap-6">
     <BookNowBtn />
+    <div class="w-11/12 flex items-center justify-around gap-6 flex-col">
+      <p class="text-neutral shadow shadow-primary p-4">
+        {{ $t("about.1") }}
+      </p>
+      <p class="text-neutral shadow shadow-primary p-4">
+        {{ $t("about.2") }}
+      </p>
+      <p class="text-neutral shadow shadow-primary p-4">
+        {{ $t("about.3") }}
+      </p>
 
-    <p class="text-neutral">
-      Welcome to Dream Style, where all your hair dreams come true! We are not
-      just your ordinary hair salon, we are a team of passionate hairstylists
-      who are dedicated to creating your dream hairstyle and giving you the
-      ultimate hair makeover experience. Step into our salon and be ready to be
-      pampered like never before!
-    </p>
-    <p class="text-neutral">
-      At Dream Style, we offer a wide range of services to cater to all your
-      haircare needs. Whether you are looking for a fabulous haircut, a stunning
-      hair color transformation, or a luxurious treatment to restore and nourish
-      your hair, we have got you covered. Our expert hairstylists are skilled in
-      the latest hair trends and techniques, ensuring that you leave our salon
-      with a hairstyle that not only suits you but also boosts your confidence
-      to new heights.
-    </p>
-    <p class="text-neutral">
-      Let's start with our incredible haircutting services. Our hairstylists
-      have an eye for detail and truly understand the importance of a good
-      haircut. Whether you want a classic bob, a trendy pixie cut, or long
-      luscious layers, our stylists will work their magic to give you a haircut
-      that flatters your face shape and enhances your features. They will listen
-      to your preferences and provide expert advice on the best hairstyle that
-      suits you.
-    </p>
+      <p class="text-neutral shadow shadow-primary p-4">
+        {{ $t("about.4") }}
+      </p>
 
-    <p class="text-neutral">
-      If you are looking to add a burst of color to your life, our hair coloring
-      services are just what you need. From vibrant highlights to rich and
-      dimensional balayage, our colorists are skilled in creating stunning color
-      transformations. They will work with you to select the perfect shade and
-      technique that complements your skin tone and personal style. Get ready to
-      turn heads with your new hair color!
-    </p>
+      <p class="text-neutral shadow shadow-primary p-4">
+        {{ $t("about.5") }}
+      </p>
 
-    <p class="text-neutral">
-      Not only do we excel in haircuts and color, but we also prioritize the
-      health and nourishment of your hair. Our hair treatments are designed to
-      repair, restore, and revitalize your locks. From deep conditioning
-      treatments to keratin smoothing therapies, our expert stylists will
-      customize the perfect treatment for your hair type and concerns. Step out
-      of our salon with shiny, healthy, and manageable hair that will make you
-      feel like a true superstar.
-    </p>
+      <p class="text-neutral shadow shadow-primary p-4">
+        {{ $t("about.6") }}
+      </p>
 
-    <p class="text-neutral">
-      Dream Style is not just about hair, it is about providing you with an
-      unforgettable experience. Our salon exudes a joyful and welcoming
-      ambiance, where you can relax and unwind while our stylists work their
-      magic. We believe in a personalized approach, taking the time to get to
-      know you and your hair goals, so that we can deliver the results you
-      desire.
-    </p>
-
-    <p class="text-neutral">
-      So why wait any longer? Visit Dream Style today and experience the hair
-      salon of your dreams. Our expert team and range of services await you with
-      open arms. We guarantee that you will walk out of our salon feeling
-      confident, beautiful, and ready to take on the world with your fabulous
-      hair. Come and let us make your hair dreams come true!
-    </p>
+      <p class="text-neutral shadow shadow-primary p-4">
+        {{ $t("about.7") }}
+      </p>
+    </div>
   </div>
 </template>
